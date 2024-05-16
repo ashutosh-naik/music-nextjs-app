d399475ab8590ef61bbdf48ea252090a68671362
Ashutosh Naik <naikashu2401@gmail.com>
2024-05-16 13:50:06 +0530

    Spotlight

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,9 +1,14 @@
 import Link from "next/link";
 import React from "react";
+import { Spotlight } from "./ui/Spotlight";
 
 function HeroSection() {
   return (
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
       <div className="p-4 relative z-10 w-full text-center">
         <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
           Master the art of Music
