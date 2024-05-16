cbfb87b457911c62104070cfe56edd622520f15f
Ashutosh Naik <naikashu2401@gmail.com>
2024-05-16 20:19:24 +0530

    Featured Webinars

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -21,7 +21,7 @@ function FeaturedCourses() {
   );
 
   return (
-    <div className="py-12 bg-gray-900">
+    <div className="p-12 bg-gray-900">
       <div>
         <div className="text-center">
           <h2 className="text-base text-teal-600 font-semibold tracking-wide uppercase">
--- src/components/TestimonialCards.tsx
+++ src/components/TestimonialCards.tsx
@@ -67,7 +67,7 @@ const musicSchoolTestimonials = [
 
 function TestimonialCards() {
   return (
-    <div className="h-[40rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
+    <div className="h-[40rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.1] bg-grid-black/[0.1] relative flex flex-col items-center justify-center overflow-hidden">
       <h2 className="text-3xl font-bold text-center mb-8 z-10">
         Hear Our Harmony: Voices of Success
       </h2>
