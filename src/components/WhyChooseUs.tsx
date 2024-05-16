e7284633016bde1158fc4db1fc30a5d67707c2ce
Ashutosh Naik <naikashu2401@gmail.com>
2024-05-16 18:04:16 +0530

    Designing Sticky Scroll

--- src/components/WhyChooseUs.tsx
+++ src/components/WhyChooseUs.tsx
@@ -4,24 +4,24 @@ import { StickyScroll } from "./ui/sticky-scroll-reveal";
 
 const musicSchoolContent = [
   {
-    title: "Collaborative Editing",
+    title: "Discover your sound with us: A personal journey in music mastery",
     description:
-      "Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.",
+      "Embark on a personalized musical adventure, refining your unique sound through tailored guidance and immersive learning experiences, fostering true mastery in your chosen instrument or genre.",
   },
   {
-    title: "Real time changes",
+    title: "Live feedback & Engagement",
     description:
-      "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
+      "Receive real-time feedback from experienced instructors, ensuring an interactive and engaging learning process that enhances your skills through immediate corrections and constructive critique.",
   },
   {
-    title: "Version control",
+    title: "Cutting-Edge Curriculum",
     description:
-      "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+      "Access a curriculum designed with the latest advancements in music education, blending traditional techniques with modern technology to provide a comprehensive and progressive learning experience.",
   },
   {
-    title: "Running out of content",
+    title: "Limitless learning opportunities",
     description:
-      "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+      "Explore an expansive range of learning resources and opportunities, enabling you to grow continuously and reach new heights in your musical journey without boundaries.",
   },
 ];
 
