cb164c454da29c1b7b57e84dcd2d19f894a34f03
Ashutosh Naik <naikashu2401@gmail.com>
2024-05-16 16:53:10 +0530

    Building featured Card section

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -1,6 +1,8 @@
+"use client";
 import React from "react";
 import courseData from "../data/music_courses.json";
 import Link from "next/link";
+import { BackgroundGradient } from "./ui/background-gradient";
 
 interface Course {
   id: number;
@@ -34,7 +36,22 @@ function FeaturedCourses() {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
           {featuredCourses.map((course: Course) => (
             <div key={course.id} className="flex justify-center">
-              test
+              <BackgroundGradient className="flex flex-col rounded-[22px] max-w-sm bg-white dark:bg-zinc-900 overflow-hidden h-full">
+                <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
+                  <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+                    {course.title}
+                  </p>
+                  <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">
+                    {course.description}
+                  </p>
+                  <Link
+                    href={`/courses/${course.slug}`}
+                    className="mt-6 px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200"
+                  >
+                    Learn more
+                  </Link>
+                </div>
+              </BackgroundGradient>
             </div>
           ))}
         </div>
