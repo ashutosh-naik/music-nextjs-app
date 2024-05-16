6398a0b0c9899a55de92de3ee95f4f68821789e0
Ashutosh Naik <naikashu2401@gmail.com>
2024-05-16 21:37:24 +0530

    Card Hover effect

--- src/components/UpcomingWebinars.tsx
+++ src/components/UpcomingWebinars.tsx
@@ -1,5 +1,46 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { HoverEffect } from "./ui/card-hover-effect";
+
+export const featuredWebinars = [
+  {
+    title: "Understanding Music Theory",
+    description:
+      "Grasp the fundamentals of music theory to enhance your comprehension and execution of musical concepts.",
+    link: "/",
+  },
+  {
+    title: "The Art of Songwriting",
+    description:
+      "Unlock your creativity by learning techniques and strategies for crafting memorable and impactful songs.",
+    link: "/",
+  },
+  {
+    title: "Mastering Your Instrument",
+    description:
+      "Elevate your skills with advanced techniques and disciplined practice tailored to mastering your musical instrument.",
+    link: "/",
+  },
+  {
+    title: "Music Production Essentials",
+    description:
+      "Explore the core elements of music production, from recording to mixing, to create polished, professional tracks.",
+    link: "/",
+  },
+  {
+    title: "Live Performance Techniques",
+    description:
+      "Improve your stage presence and performance skills to captivate and engage live audiences effectively.",
+    link: "/",
+  },
+  {
+    title: "Digital Music Marketing",
+    description:
+      "Discover strategies to promote your music online, grow your audience, and build your brand in the digital age.",
+    link: "/",
+  },
+];
 
 function UpcomingWebinars() {
   return (
@@ -13,7 +54,11 @@ function UpcomingWebinars() {
             Enhance Your Musical Journey
           </p>
         </div>
-        <div className="mt-10">2</div>
+        {/* <div className="mt-10"> */}
+        <div className="mt-10 max-w-6xl mx-auto px-8">
+          <HoverEffect items={featuredWebinars} />
+        </div>
+        {/* </div> */}
         <div className="mt-10 text-center">
           <Link
             href="/webinars"
