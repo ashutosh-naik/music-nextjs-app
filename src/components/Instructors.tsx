ad59bc5b35391c721b58c9f2d35e26ee005c17f1
Ashutosh Naik <naikashu2401@gmail.com>
2024-05-16 22:47:39 +0530

    Animated Tool-tip and Wavy Background

--- src/components/Instructors.tsx
+++ src/components/Instructors.tsx
@@ -1,5 +1,38 @@
+"use client";
 import React from "react";
 import { WavyBackground } from "./ui/wavy-background";
+import { AnimatedTooltip } from "./ui/animated-tooltip";
+
+const instructors = [
+  {
+    id: 1,
+    name: "James Anderson",
+    designation: "Vocal Coach",
+    image:
+      "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+  },
+  {
+    id: 2,
+    name: "Michael Davis",
+    designation: "Guitar Instructor",
+    image:
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 3,
+    name: "Emily Johnson",
+    designation: "Piano Teacher",
+    image:
+      "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 4,
+    name: "Olivia Martinez",
+    designation: "Drumming Expert",
+    image:
+      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+];
 
 function Instructors() {
   return (
@@ -8,11 +41,13 @@ function Instructors() {
         <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold text-center">
           Meet Our Instructors
         </p>
-        <p className="text-base md:text-lg mt-4 text-white font-normal text-center">
+        <p className="text-base md:text-lg mt-8 mb-4 text-white font-normal text-center">
           Discover the talented professionals who will guide your musical
           journey
         </p>
-        <div className=""></div>
+        <div className="flex flex-row items-center justify-center mb-10 w-full">
+          <AnimatedTooltip items={instructors} />
+        </div>
       </WavyBackground>
     </div>
   );
