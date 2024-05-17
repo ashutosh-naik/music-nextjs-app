4461bce9ec08a32906880dacb85bf1bfee354603
Ashutosh Naik <naikashu2401@gmail.com>
2024-05-17 14:09:40 +0530

    Music School App

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -42,18 +42,17 @@ function Courses() {
                 <CardItem
                   translateZ={20}
                   as={Link}
-                  href="https://twitter.com/mannupaaji"
-                  target="__blank"
+                  href="/courses"
                   className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
                 >
-                  Try now →
+                  Instructor: {course.instructor}
                 </CardItem>
                 <CardItem
                   translateZ={20}
                   as="button"
                   className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
                 >
-                  Sign up
+                  ${course.price}
                 </CardItem>
               </div>
             </CardBody>
